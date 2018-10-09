bbe3325b3f007cdbc99602b3c92eda50d6ef2640
Michael Weller <vve11ermichae1@gmail.com>
2018-10-09 14:12:42 +0100

    Fixed conflict in student model with course list

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,6 @@ class Student < ApplicationRecord
   belongs_to :group
   has_many :attendances
 
-<<<<<<< HEAD
   courses = []
   groups = Group.all
 
@@ -11,10 +10,6 @@ class Student < ApplicationRecord
     courses.push output
   end
 
-
-  COURSE_LIST = courses
-=======
   COURSE_LIST = ["Devops", "SDET", "Business Analytics"]
 
->>>>>>> a0d6aa48058801f9c744bb26cfc463f3ea9a96c3
 end
