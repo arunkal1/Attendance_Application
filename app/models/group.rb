5b52784a9682b96078cbc42dc9b43068b5db0d52
Bakang <bakang97@gmail.com>
2018-10-09 10:14:09 +0100

    made form and set up data validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,9 @@
 class Group < ApplicationRecord
   belongs_to :students
+
+  validates :name, presence: { message: "name must be given" }
+  validates :type, presence: { message: "type must be given" }
+  validates :startdate, presence: { message: "start date must be given" }
+  validates :enddate, presence: { message: "end date must be given" }
+  validates :active, presence: { message: "active must be given" } 
 end
