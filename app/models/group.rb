e2f5acf47d4e37616623666c149de73b1f01623e
Bakang <bakang97@gmail.com>
2018-10-09 10:26:08 +0100

    fixed form error

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,7 +4,7 @@ class Group < ApplicationRecord
 
 
   validates :name, presence: { message: "name must be given" }
-  validates :type, presence: { message: "type must be given" }
+  validates :stream, presence: { message: "stream must be given" }
   validates :startdate, presence: { message: "start date must be given" }
   validates :enddate, presence: { message: "end date must be given" }
   validates :active, presence: { message: "active must be given" }
