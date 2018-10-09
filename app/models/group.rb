1800afb49313b096d9440e88199db27cb213b415
Bakang <bakang97@gmail.com>
2018-10-09 13:47:13 +0100

    added a drop down from the active option for the groups form

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,11 +2,12 @@ class Group < ApplicationRecord
 
   has_many :students
 
+  active_option = [true,false]
 
   validates :name, presence: { message: "name must be given" }
   validates :stream, presence: { message: "stream must be given" }
   validates :startdate, presence: { message: "start date must be given" }
   validates :enddate, presence: { message: "end date must be given" }
 
-
+ ACTIVE_OPTION = active_option
 end
