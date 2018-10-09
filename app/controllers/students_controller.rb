ecac73b82c70a0064366ac4c5b4f55496a991398
Elisha <eharmison@spartaglobal.com>
2018-10-09 10:34:35 +0100

    students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,22 +1,29 @@
 class StudentsController < ApplicationController
   def index
+    @student  = Student.all
   end
 
   def show
+    @student = Student.find params[:id]
   end
 
   def new
+    @student = Student.new
   end
 
   def edit
+    @student = Student.find params[:id]
   end
 
   def create
+    @student = Student.new
   end
 
   def update
+    @student = Student.find params[:id]
   end
 
   def destroy
+    @student = Student.find params[:id]
   end
 end
