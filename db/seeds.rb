eb82478c03842429dfc81b18d6568b99846d5091
Michael Weller <vve11ermichae1@gmail.com>
2018-10-09 10:11:57 +0100

    Created mock students in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Student.create(name: "Rose Lane", active: true, group_id:1, created_at:"2018-10-09", updated_at:"2018-10-09")
+
+  Student.create(name: "Christian Dixon", active: true, group_id:1, created_at:"2018-10-09", updated_at:"2018-10-09")
+
+  Student.create(name: "John Bates", active: true, group_id:1, created_at:"2018-10-09", updated_at:"2018-10-09")
+
+  Student.create(name: "Gloria Roberts", active: true, group_id:1, created_at:"2018-10-09", updated_at:"2018-10-09")
