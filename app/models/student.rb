007ebae6e0285014e6c1fc509fd5848ba540e8d5
tech-a58 <rupinder.lall@outlook.com>
2018-10-09 13:48:16 +0100

    Created a dropdown for group select

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,15 @@
 class Student < ApplicationRecord
   belongs_to :group
   has_many :attendances
+
+  courses = []
+  groups = Group.all
+
+  groups.each do |group|
+    output = "#{group.id} - #{group.name}"
+    courses.push output
+  end
+
+
+  COURSE_LIST = courses
 end
