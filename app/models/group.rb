3f40abf0d2ee36d9551d8f0d593e772063692d82
Michael Weller <vve11ermichae1@gmail.com>
2018-10-09 12:10:55 +0100

    Changed typo in models, added to show and index and fixed schema and seeds

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,3 @@
 class Group < ApplicationRecord
-  has_many :students
+  has_many :student
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  Student.create(name: "Rose Lane", active: true, group_id:1, created_at:"2018-10-09", updated_at:"2018-10-09")
+Group.create(name: 'Eng-16', stream: 'sdet', startdate: '21/08/2018', enddate: '22/11/2018', active: true)
 
-  Student.create(name: "Christian Dixon", active: true, group_id:1, created_at:"2018-10-09", updated_at:"2018-10-09")
+Student.create(name: 'Robin', active: true, group_id: 1)
+Student.create(name: 'Rhys', active: true, group_id: 1)
+Student.create(name: 'Dan', active: true, group_id: 1)
+Student.create(name: 'Tom', active: true, group_id: 1)
+Student.create(name: 'Arun', active: true, group_id: 1)
 
-  Student.create(name: "John Bates", active: true, group_id:1, created_at:"2018-10-09", updated_at:"2018-10-09")
 
-  Student.create(name: "Gloria Roberts", active: true, group_id:1, created_at:"2018-10-09", updated_at:"2018-10-09")
+Attendance.create(date: '01/01/2018', att_status: 1, student_id: 1)
+Attendance.create(date: '01/01/2018', att_status: 2, student_id: 2)
+Attendance.create(date: '01/01/2018', att_status: 3, student_id: 3)
+Attendance.create(date: '01/01/2018', att_status: 4, student_id: 4)
+Attendance.create(date: '01/01/2018', att_status: 5, student_id: 5)
