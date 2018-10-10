7c0b4ce99338dbfa6ec3faf3d2485abbea010300
Bakang <bakang97@gmail.com>
2018-10-10 10:10:55 +0100

    added styles to buttons to show how the data is sorted

--- app/assets/javascripts/sortby.js
+++ app/assets/javascripts/sortby.js
@@ -1,29 +1,55 @@
 document.addEventListener('DOMContentLoaded',function(){
 
-
-
+  // if ($("#order_name").is(":visible") && ($("#order_id").is(":visible"))){
+  //
+  //   $("#order_name").hide();
+  //
+  // }
+  $("#order_id").show()
   $("#order_name").hide();
+  $("#order_active").hide();
 
+  document.getElementById("sort_name").addEventListener("click", function(){
+    $("#order_name").show();
+    $("#order_id").hide();
+    $("#order_active").hide();
+    // $("#sort_name").css({"background-color":"black", "color":"white"})
+    // $("#group_info").load(location.href+" #group_info>*","");
+  });
 
-  if ($("#order_name").is(":visible") && ($("#order_id").is(":visible"))){
-
+  document.getElementById("sort_id").addEventListener("click", function(){
     $("#order_name").hide();
+    $("#order_id").show();
+    $("#order_active").hide();
 
-  }
+    // $("#group_info").load(location.href+" #group_info>*","");
+  });
+  document.getElementById("sort_active").addEventListener("click", function(){
+    $("#order_name").hide();
+    $("#order_id").hide();
+    $("#order_active").show();
+    // $("#group_info").load(location.href+" #group_info>*","");
+  });
+    setInterval(function () {
 
-    document.getElementById("sort_name").addEventListener("click", function(){
-      $("#order_name").show();
-      $("#order_id").hide();
-      console.log("pressed");
-      // $("#group_info").load(location.href+" #group_info>*","");
-    });
+      if ($("#order_name").is(":visible")) {
+        $("#sort_name").css({"background-color":"black", "color":"white"})
+      } else {
+        $("#sort_name").css({"background-color":"#6c757d", "color":"white"})
+      }
+      if ($("#order_active").is(":visible")) {
+        $("#sort_active").css({"background-color":"black", "color":"white"})
+      } else {
+        $("#sort_active").css({"background-color":"#6c757d", "color":"white"})
+      }
+      if ($("#order_id").is(":visible")) {
+        $("#sort_id").css({"background-color":"black", "color":"white"})
+      } else {
+        $("#sort_id").css({"background-color":"#6c757d", "color":"white"})
+      }
 
-    document.getElementById("sort_id").addEventListener("click", function(){
-      $("#order_name").hide();
-      $("#order_id").show();
-      // $("#group_info").load(location.href+" #group_info>*","");
+    }, 10);
 
-    });
 
 
 });
