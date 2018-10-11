959d764928caec0b1db4af4c12fd1ec1513376de
Michael Weller <vve11ermichae1@gmail.com>
2018-10-11 10:41:48 +0100

    Fixed a bug so that when a group is deleted all students in that group are also deleted

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -45,6 +45,9 @@ class GroupsController < ApplicationController
 
   def destroy
     @group = Group.find params[:id]
+    @group.students.each do |student|
+      student.destroy
+    end
     @group.destroy
     redirect_to root_path
   end
